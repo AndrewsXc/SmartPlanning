73b2aacd9514d0fe839dfbbffb0a8bd09a179517
flylxc <894863383@qq.com>
2018-09-24 17:14:58 +0800

    MainMenuViewController

---
+++ Podfile
@@ -0,0 +1,14 @@
+project 'SmartPlanning.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SmartPlanning' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for SmartPlanning
+  pod "AFNetworking"
+  pod "iCarousel"
+  pod "FXImageView"
+end
--- SmartPlanning/MainMenuView/LXCMainMenuViewController.m
+++ SmartPlanning/MainMenuView/LXCMainMenuViewController.m
@@ -7,16 +7,29 @@
 //
 
 #import "LXCMainMenuViewController.h"
-
-@interface LXCMainMenuViewController ()
-
+#import <iCarousel.h>
+#import <FXImageView.h>
+typedef NS_ENUM(NSInteger, ViewControllerType)
+{
+    ViewControllerTypeCityOverView      = 0,
+    ViewControllerTypeGeneralPlan,
+    ViewControllerTypeCityConstructFormerYear,
+    ViewControllerTypeCityConstructLatterYear,
+    ViewControllerTypeMapView,
+    ViewControllerTypeDocument,
+    ViewControllerTypeAboutUs,
+};
+@interface LXCMainMenuViewController ()<iCarouselDataSource,iCarouselDelegate>
+@property (weak, nonatomic) IBOutlet iCarousel *mainView;
+@property (nonatomic, strong) NSMutableArray *items;
+@property (nonatomic, assign) ViewControllerType currentViewControllerType;
 @end
 
 @implementation LXCMainMenuViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setMainView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +46,49 @@
     // Pass the selected object to the new view controller.
 }
 */
+-(void)setMainView{
+    UIImage * bgimage = [UIImage imageNamed:@"MainBackground"];
+    [_mainView setBackgroundColor:[UIColor clearColor]];
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:bgimage]];
+    _currentViewControllerType = 0;
+    _mainView.scrollSpeed = 10;
+    _mainView.scrollToItemBoundary = YES;
+    _mainView.type = iCarouselTypeCoverFlow;
+    _mainView.dataSource = self;
+    _mainView.delegate = self;
+   
+}
+
+#pragma mark iCarousel
+- (void)carouselCurrentItemIndexDidChange:(iCarousel *)carousel{
+    _currentViewControllerType = [_mainView currentItemIndex];
+}
+- (nonnull UIView *)carousel:(nonnull iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(nullable UIView *)view {
+    if(view == nil)//nil OC对象 Nil 自定义类对象
+    {
+        FXImageView* imageView = [[FXImageView alloc] initWithFrame:CGRectMake(0, 0, 600.f, 300.f) ];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        imageView.asynchronous = YES;
+        imageView.reflectionScale = 0.5f;
+        imageView.reflectionAlpha = 0.25f;
+        imageView.reflectionGap = 10.0f;
+        imageView.cornerRadius = 10.0f;
+        view = imageView;
+    }
+    UIImage *image=[UIImage imageNamed:_items[index]];
+    [view setValue:image forKey:@"image"];
+    return view;
+}
+
+- (NSInteger)numberOfItemsInCarousel:(nonnull iCarousel *)carousel {
+    _items = [@[@"CityOverView",
+                @"GeneralPlan",
+                @"CityConstructFormerYear",
+                @"CityConstructLatterYear",
+                @"MapView",
+                @"Document",
+                @"AboutUs"] mutableCopy];
+    return [_items count];
+}
 
 @end
