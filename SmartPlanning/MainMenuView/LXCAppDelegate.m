393fae75e2730c87705748971e098e3208f089c0
flylxc <630508515@qq.com>
2018-09-24 11:54:18 +0800

    Appdelegate

--- SmartPlanning/MainMenuView/LXCAppDelegate.m
+++ SmartPlanning/MainMenuView/LXCAppDelegate.m
@@ -1,18 +1,18 @@
 //
-//  AppDelegate.m
+//  LXCAppDelegate.m
 //  SmartPlanning
 //
 //  Created by yaqiannnn on 2018/9/24.
 //  Copyright © 2018年 NJU. All rights reserved.
 //
 
-#import "AppDelegate.h"
+#import "LXCAppDelegate.h"
 
-@interface AppDelegate ()
+@interface LXCAppDelegate ()
 
 @end
 
-@implementation AppDelegate
+@implementation LXCAppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
