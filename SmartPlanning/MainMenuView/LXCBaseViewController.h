3bb1b95ac4798b06135d9f559bf65e75e116be1a
flylxc <894863383@qq.com>
2018-09-24 14:06:00 +0800

    BaseView

---
+++ SmartPlanning/MainMenuView/LXCBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  LXCBaseViewController.h
+//  SmartPlanning
+//
+//  Created by yaqiannnn on 2018/9/24.
+//  Copyright © 2018年 NJU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface LXCBaseViewController : ViewController
+
+@end
---
+++ SmartPlanning/MainMenuView/LXCMainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  LXCMainMenuViewController.h
+//  SmartPlanning
+//
+//  Created by yaqiannnn on 2018/9/24.
+//  Copyright © 2018年 NJU. All rights reserved.
+//
+
+#import "LXCBaseViewController.h"
+
+@interface LXCMainMenuViewController : LXCBaseViewController
+
+@end
