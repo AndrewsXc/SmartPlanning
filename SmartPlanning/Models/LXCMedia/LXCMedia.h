c77b18bdd837d35b23fc333c5743c6808e3cb594
flylxc <894863383@qq.com>
2018-10-25 21:26:14 +0800

    20181025

---
+++ SmartPlanning/Models/LXCMedia/LXCMedia.h
@@ -0,0 +1,13 @@
+//
+//  LXCMedia.h
+//  SmartPlanning
+//
+//  Created by yaqiannnn on 2018/9/26.
+//  Copyright © 2018年 NJU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LXCMedia : NSObject
+
+@end
---
+++ SmartPlanning/Models/LXCMedia/LXCMedia.m
@@ -0,0 +1,13 @@
+//
+//  LXCMedia.m
+//  SmartPlanning
+//
+//  Created by yaqiannnn on 2018/9/26.
+//  Copyright © 2018年 NJU. All rights reserved.
+//
+
+#import "LXCMedia.h"
+
+@implementation LXCMedia
+
+@end
