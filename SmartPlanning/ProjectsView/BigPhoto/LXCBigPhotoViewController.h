1e79cba87a944efb63ee24f9a218e9480c455c3d
flylxc <894863383@qq.com>
2018-11-20 14:22:16 +0800

    20181120_PhotoView

---
+++ SmartPlanning/ProjectsView/BigPhoto/LXCBigPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  LXCBigPhotoViewController.h
+//  SmartPlanning
+//
+//  Created by yaqiannnn on 2018/11/20.
+//  Copyright © 2018年 NJU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXCBigPhotoViewController : UIPageViewController
+
+@end
---
+++ SmartPlanning/ProjectsView/BigPhoto/LXCImageScrollView.m
@@ -0,0 +1,21 @@
+//
+//  LXCImageScrollView.m
+//  SmartPlanning
+//
+//  Created by yaqiannnn on 2018/11/20.
+//  Copyright © 2018年 NJU. All rights reserved.
+//
+
+#import "LXCImageScrollView.h"
+
+@implementation LXCImageScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
